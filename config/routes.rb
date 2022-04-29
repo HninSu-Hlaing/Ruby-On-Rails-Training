48794c18bde6e61069a9af2b7849eeec367b4660
LAPTOP-CAR9KD80\wp <hninsuhlaing2282020@gmail.com>
2022-04-29 15:58:21 +0630

    add tuto11 file

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,13 @@ Rails.application.routes.draw do
   delete "logout", to: "sessions#destroy"
   get "tuto9", to: "store#store_img"
   post "tuto9", to: "store#create"
+  get "tuto11" , to: "pages#qr_code_generator"
+
+  controller :pages do
+    get :qr_code_generator
+    get :qr_code_download
+    get :qr_show
+  end
   #resources :users, except: [:new]
   root to: "main#index"
 end
