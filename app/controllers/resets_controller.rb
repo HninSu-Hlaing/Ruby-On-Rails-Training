45cbb5dad1c8e32d9668ba8ac2d08ef5455faea8
LAPTOP-CAR9KD80\wp <hninsuhlaing2282020@gmail.com>
2022-05-04 16:55:49 +0630

    update reset password file for tuto14

--- app/controllers/resets_controller.rb
+++ app/controllers/resets_controller.rb
@@ -8,6 +8,23 @@ class ResetsController < ApplicationController
             PasswordMailer.with(user:@user).reset.deliver_now
         end
         redirect_to root_path
-        flash[:notice] = 'If an account with the email is found, send email with reser password link'
+        flash[:notice] = 'If an account with the email is found, send email with reset password link'
+    end
+    def edit
+        @user = User.find_signed(params[:token],purpose:"password_reset")
+        rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to sign_in_path,notice: "Your token has been expired.Please try again"
+    end
+    def update
+        @user = User.find_signed(params[:token],purpose:"password_reset")
+        if @user.update(password_params)
+            redirect_to sign_in_path,notice:"Your password was successfully reset.Please sign in"
+        else 
+            render :edit
+        end
+    end
+    private
+    def password_params
+        params.require(:user).permit(:password)
     end
 end
